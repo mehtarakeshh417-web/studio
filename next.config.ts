eea1005c7312af0220d35cc9181992c26fb87f3f
Rakesh Mehta <mehta.rakesh417@gmail.com>
2025-11-02 16:36:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  env: {
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-  },
   images: {
     remotePatterns: [
       {
