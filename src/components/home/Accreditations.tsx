2cb53dba6e631099d5b38ab03d733b008b73f16c
Mmehta Rakesh <mmehta.rakesh417@gmail.com>
2025-11-02 13:24:51 +0000

    enlarge 120% more

--- src/components/home/Accreditations.tsx
+++ src/components/home/Accreditations.tsx
@@ -40,7 +40,7 @@ export function Accreditations() {
               href={accreditation.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="relative h-12 w-24 hover:opacity-80 transition-opacity duration-300"
+              className="relative h-28 w-56 hover:opacity-80 transition-opacity duration-300"
             >
               <Image
                 src={accreditation.imageUrl}
