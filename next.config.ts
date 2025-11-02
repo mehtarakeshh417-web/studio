da0c20a0083329376949dc492e9a3aa48e2e96be
Mmehta Rakesh <mmehta.rakesh417@gmail.com>
2025-11-02 14:09:34 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
